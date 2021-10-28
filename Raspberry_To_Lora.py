0ce56d7dee93fe8c902e360093bff49109393f62
kelvin169 <kelvinmwaniki8@gmail.com>
2021-10-28 23:56:43 +0300

    This was the first commit

---
+++ Raspberry_To_Lora.py
@@ -0,0 +1,39 @@
+from time import sleep
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+BOARD.setup()
+
+from time import sleep
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+BOARD.setup()
+
+ def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        while True:
+            sleep(.5)
+            rssi_value = self.get_rssi_value()
+            status = self.get_modem_status()
+            sys.stdout.flush()
+
+             def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        while True:
+            sleep(.5)
+            rssi_value = self.get_rssi_value()
+            status = self.get_modem_status()
+            sys.stdout.flush()
+
+            try:
+    lora.start()
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    print("")
+    sys.stderr.write("KeyboardInterrupt\n")
+finally:
+    sys.stdout.flush()
+    print("")
+    lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
